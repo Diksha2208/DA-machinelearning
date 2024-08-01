0f7a1da527bacc67aefcadc20264647fd85bcaee
Diksha2208 <44769186+Diksha2208@users.noreply.github.com>
2024-08-01 15:15:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Machine Learning App')
 
 st.write('This app builds a machine learning model')
 
-df = pd.read_csv('https://raw.githubusercontent.com/Diksha2208/DA-machinelearning/master/data/Global_YouTube_Statistics.csv')
+df = pd.read_csv('data/Global_YouTube_Statistics.csv')
 df
                  
                  
