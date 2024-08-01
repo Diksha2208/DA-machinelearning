92011943fe5ce0ac5a445d423784ddcbcc51c98c
Diksha2208 <44769186+Diksha2208@users.noreply.github.com>
2024-08-01 15:17:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ import pandas as pd
 st.title('Machine Learning App')
 
 st.write('This app builds a machine learning model')
-
-df = pd.read_csv('data/Global_YouTube_Statistics.csv')
-df
-                 
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('data/Global_YouTube_Statistics.csv')
+  df
+                     
                  
