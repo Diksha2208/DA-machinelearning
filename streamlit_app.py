0949fa11f6b89a980e03b17badfd3cc870754358
Diksha2208 <44769186+Diksha2208@users.noreply.github.com>
2024-08-01 14:52:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import panda as pd
 
 st.title('Machine Learning App')
 
 st.write('This app builds a machine learning model')
 
+df = pd.read_csv(https://github.com/Diksha2208/DA-machinelearning/blob/master/data/Global_YouTube_Statistics.csv')
+df
+                 
+                 
