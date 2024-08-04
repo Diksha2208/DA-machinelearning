6102d59baca745da6b0e4a595298add163a67b9b
Diksha2208 <44769186+Diksha2208@users.noreply.github.com>
2024-08-04 13:00:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,35 @@
-import streamlit as st
 import pandas as pd
+import pandas_profiling
+import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
+
+option = st.sidebar.selectbox("Choose a dataset:", ("Load a dataset", "Use example dataset"))
+
+if option == "Load a dataset":
+    uploaded_file = st.sidebar.file_uploader("Upload a dataset (CSV file)", type=["csv"])
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        # perform data profiling using Pandas-Profiling
+        pr = df.profile_report()
+        st.header("**Dataset:**")
+        st.write(df)
+        if st.sidebar.button("Generate Report"):
+            st.write("---")
+            st.header("**Pandas Profiling Report**")
+            st_profile_report(pr)
+
+if option == "Use example dataset":
+    dataset = st.sidebar.selectbox("Try a preloaded dataset:", ("Diabetes dataset", "Chronic Kidney Disease Dataset"))
+    if dataset == "Youtubedataset":
+        df = pd.read_csv("data/Global_YouTube_Statistics.csv")
+        # perform data profiling using Pandas-Profiling
+        pr = df.profile_report()
+        st.header("**Dataset:**")
+        st.write(df)
+        if st.sidebar.button("Generate Report"):
+            st.write("---")
+            st.header("**Pandas Profiling Report**")
+            st_profile_report(pr)
 
 @st.cache_data
 def load_data():
