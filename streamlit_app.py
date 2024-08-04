cfc8cfe04b3f94798647ff3481355cc4e401c9c2
Diksha2208 <44769186+Diksha2208@users.noreply.github.com>
2024-08-04 13:15:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import pandas as pd
-import pandas_profiling
 import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
+
 
 # Function to load the dataset
 @st.cache
@@ -24,20 +23,15 @@ if option == "Load a dataset":
         if st.sidebar.button("Generate Report"):
             st.write("---")
             st.header("**Pandas Profiling Report**")
-            st_profile_report(pr)
+            
 
 # Use an example dataset
 elif option == "Use example dataset":
     dataset = st.sidebar.selectbox("Select an example dataset:", ("Diabetes dataset", "Chronic Kidney Disease Dataset", "YouTube dataset"))
     
-    if dataset == "Diabetes dataset":
-        df = load_example_data("data/diabetes.csv")  # Replace with actual path to your dataset
-    elif dataset == "Chronic Kidney Disease Dataset":
-        df = load_example_data("data/chronic_kidney_disease.csv")  # Replace with actual path to your dataset
-    elif dataset == "YouTube dataset":
+    if dataset == "YouTube dataset":
         df = load_example_data("data/Global_YouTube_Statistics.csv")  # Replace with actual path to your dataset
     
-    pr = df.profile_report()
     st.header("**Dataset:**")
     st.write(df)
     if st.sidebar.button("Generate Report"):
